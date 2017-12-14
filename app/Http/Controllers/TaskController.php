56ae8d6228ecd1c49802fcb8d21703042f7f617a
at-duongtruong <duong.truong@asiantech.vn>
2017-12-14 15:26:18 +0700

    Add Tasks And Validation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,4 +10,12 @@ class TaskController extends Controller
 		public function __construct(){
 			$this->middleware('auth');
 		}
+		public function index(Request $request){
+			return view('tasks.index');
+		}
+		public function store(Request $request){
+			$this->validate($request,[
+				'name' => 'required | max:255',
+			]);
+		}
 }
